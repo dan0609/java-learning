893693187884f6ea1abff437d71bbf564fb52828
dan0609 <13564301371@163.com>
2020-12-03 22:27:13 +0800

    feature/init

---
+++ src/L1HelloWorld.java
@@ -0,0 +1,5 @@
+public class L1HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld哒哒哒!");
+    }
+}
---
+++ src/L2HelloWorld.java
@@ -0,0 +1,5 @@
+public class L2HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
